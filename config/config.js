7310e1dfc23bdbef85cdb68f71399343dafc1ef0
Pranav <pranavmendi@gmail.com>
2021-05-13 02:56:46 +0530

    Setup Firebase's Firestore API to save data & set port using assert module

---
+++ config/config.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+require('dotenv').config();
+
+const { PORT, HOST, HOST_URL } = process.env;
+
+assert(PORT, 'PORT is required');
+assert(HOST, 'HOST is required');
+
+module.exports = { port: PORT, host: HOST, url: HOST_URL };
---
+++ db/firebaseDB.js
@@ -0,0 +1,23 @@
+const firebase = require('firebase');
+require('dotenv').config();
+
+const {
+	API_KEY,
+	AUTH_DOMAIN,
+	PROJECT_ID,
+	STORAGE_BUCKET,
+	MESSAGING_SENDER_ID,
+	APP_ID,
+} = process.env;
+
+const firebaseConfig = {
+	apiKey: API_KEY,
+	authDomain: AUTH_DOMAIN,
+	projectId: PROJECT_ID,
+	storageBucket: STORAGE_BUCKET,
+	messagingSenderId: MESSAGING_SENDER_ID,
+	appId: APP_ID,
+};
+
+const db = firebase.initializeApp(firebaseConfig);
+module.exports = db;
