2260ae092dfb946089176bbe3a9730ed14a5e52f
Pranav <pranavmendi@gmail.com>
2021-05-13 03:00:37 +0530

    Add get and post request controllers for home route

---
+++ controllers/home_controller.js
@@ -0,0 +1,25 @@
+const firebase = require('../db/firebaseDB');
+const firestore = firebase.firestore();
+
+module.exports.getHome = (req, res) => {
+	try {
+		res.render('home.html');
+	} catch (err) {
+		res.status(400).send(err.message);
+	}
+};
+
+module.exports.postHome = async (req, res) => {
+	try {
+		const { name, email, message } = req.body;
+		console.log({ name, email, message });
+		await firestore
+			.collection('users')
+			.doc()
+			.set({ name: name, email: email, message: message });
+		return res.send('Successful');
+		// return res.redirect('back');
+	} catch (err) {
+		res.status(400).send(err.message);
+	}
+};
