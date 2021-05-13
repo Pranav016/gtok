b230593ff353080587dab3e94f7f8220106cfc93
Pranav <pranavmendi@gmail.com>
2021-05-14 01:59:29 +0530

    Add ajax request to submit form data

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,34 @@
-$('#form').on('submit', (e) => {
-	swal({
-		title: 'Success!',
-		text: 'Thank you for filling the form!',
-		icon: 'success',
+$(document).ready(() => {
+	$('#form').on('submit', (e) => {
+		e.preventDefault();
+		const name = $('#name').val();
+		const email = $('#email').val();
+		const message = $('#message').val();
+		console.log({ name, email, message });
+		$.ajax({
+			url: '/',
+			data: {
+				name: name,
+				email: email,
+				message: message,
+			},
+			method: 'POST',
+			contentType: 'application/x-www-form-urlencoded',
+			success: () => {
+				swal({
+					title: 'Success!',
+					text: 'Thank you for filling the form!',
+					icon: 'success',
+				});
+			},
+			error: (err) => {
+				swal({
+					title: 'Error!',
+					text: 'Try again later!',
+					icon: 'error',
+				});
+				console.log(err);
+			},
+		});
 	});
 });
