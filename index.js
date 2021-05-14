11439aca0e9075beb97d8299c8ca91e31f587875
Pranav <pranavmendi@gmail.com>
2021-05-14 11:47:52 +0530

    Configure firebase

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const config = require('./config/config');
 const indexRoute = require('./routes/index');
+/* const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+admin.initializeApp();
+const { SENDER_EMAIL, SENDER_PASSWORD } = process.env; */
 
 const app = express();
 
@@ -18,3 +25,33 @@ app.listen(config.port, (err) => {
 	}
 	console.log(`Server started on port: ${config.port}`);
 });
+
+/* exports.sendEmailNotification = functions.firestore
+	.document('submission/{docId}')
+	.onCreate((snap, ctx) => {
+		const data = snap.data();
+
+		let authData = nodemailer.createTransport({
+			host: 'smtp.gmail.com',
+			port: 465,
+			secure: true,
+			auth: {
+				user: SENDER_EMAIL,
+				pass: SENDER_PASSWORD,
+			},
+		});
+		authData
+			.sendMail({
+				from: 'alphavio15@gmail.com',
+				to: `${data.email}`,
+				subject: 'Thank you contacting us!',
+				html: `<div>
+		Hello User! <br/>
+		Thank you for contacting Lets Gtok. Our team will shortly respond to your request.
+		</div>
+		`,
+			})
+			.then((res) => console.log('email sent successfully'))
+			.catch((err) => console.log(err));
+	});
+*/
